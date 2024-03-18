d3f59a68ef0c53151a2bec3c0d5b081eb2d30b5e
Kevin Csiffary <kevin.csiffary@stu.ocsb.ca>
2024-03-18 14:47:48 +0000

    Made DiceGame

--- com/example/DiceGame.java
+++ com/example/DiceGame.java
@@ -1,6 +1,3 @@
-
-package com.example;
-
 /**
  * Change me.
  *
@@ -8,6 +5,8 @@ package com.example;
  * @version 1.0
  * @since 2024-03-18
  */
+
+import java.util.*;
 
 // DiceGame class
 public final class DiceGame {
@@ -23,6 +22,64 @@ public final class DiceGame {
      * @param args Unused
      */
     public static void main(final String[] args) {
-        System.out.println("Hello, world");
+
+        boolean quit = false;
+
+        int guessCounter = 0;
+
+        // Create the scanner.
+        Scanner sc = new Scanner(System.in);
+
+        Random rand = new Random();
+
+        // Tell the user what the program does.
+        System.out.print("This program is a guessing game, ");
+        System.out.println(" guess until you get the right answer!");
+
+        do {
+            // Increments guess counter
+            guessCounter++;
+            
+            // Calculate random number.
+            int randNum = rand.nextInt(6) + 1;
+
+            // Setup try catch
+            try {
+                // Get user input.
+                System.out.println("\nGuess a number between 1 and 6");
+                int guess = sc.nextInt();
+
+                // Checks if the guess is in the range
+                if (guess >= 1 && guess <= 6) {
+                    if (randNum == guess) {
+                        System.out.print("Congratulations! ");
+                        System.out.println("You guessed correctly. :)");
+                        System.out.print(" it took you " + guessCounter);
+                        System.out.println(" trie(s) to get it right!");
+                        guessCounter = 0;
+                    } else {
+                        System.out.println("Nope, thats not right! :(");
+                    }
+                } else {
+                    System.out.println("NO guess a number between 1 and 6!");
+                }
+
+                sc.nextLine();
+
+                System.out.println("\nWould you like to try again?");
+                System.out.println("Enter q to quit");
+                String userQuit = sc.nextLine();
+
+                if (userQuit.equals("q")) {
+                    break;
+                }
+
+            } catch (Exception e) {
+                System.out.println("Please enter a proper number!");
+                sc.nextLine();
+                guessCounter--;
+                continue;
+            }
+        } while (!quit);
     }
 }
